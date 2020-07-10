80b92996f0ebc46f1f825aecc0f1cd93c86235db
Kareem James <kareem.james@detroitlabs.com>
2020-07-09 21:03:11 -0400

    initial commit

---
+++ src/main/java/com/hoodwink/hoodwinkbeerservice/HoodwinkBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hoodwink.hoodwinkbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoodwinkBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoodwinkBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoodwink/hoodwinkbeerservice/HoodwinkBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoodwink.hoodwinkbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoodwinkBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
